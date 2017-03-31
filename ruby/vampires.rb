2e116cb902d07f9535fcc619cb91f4895f8b3da9
Peter Thompson <peterthompson@roamflyfishing.com>
2017-03-31 14:56:10 -0600

    Add conditional statements with booleans for questionare

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,12 +1,32 @@
+def age_validator(age, birth)
+  if age.to_i == 2017 - birth.to_i
+    age_validator = true
+  else 
+    age_validator = false
+  end
+end
+
 puts "What is your name?"
 name = gets.chomp
 
-puts "How old are you and what year were you born?"
-age_birthday = gets.chomp
+puts "How old are you?"
+age = gets.to_i
+puts "What year were you born?"
+birth = gets.to_i
+age_validator(age, birth)
 
 puts "We serve garlic bread for lunch.  Would you like some?  Yes or No"
-answer = gets.chomp
+garlic = gets.chomp
 
 puts "Would you like to be enrolled in our company's health insurance?  Yes or No"
-response = gets.chomp
+insurance = gets.chomp
 
+
+
+if age_validator = true && (garlic == "yes" || insurance == "yes")
+  puts  "Probably not a vampire."
+elsif !age_validator && (garlic == "no" || insurance == "no")
+  puts "Probably a vampire."
+elsif !age_validator && (garlic == "no" && insurance == "no")
+  puts "Almost certainly a vampire"   
+end
