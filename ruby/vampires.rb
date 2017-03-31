f2b4cfa3a43422437df0c5d0da3253aad281e5a0
Peter Thompson <peterthompson@roamflyfishing.com>
2017-03-31 11:31:21 -0600

    Create a program that asks new employee's questions.

---
+++ ruby/vampires.rb
@@ -0,0 +1,12 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you and what year were you born?"
+age_birthday = gets.chomp
+
+puts "We serve garlic bread for lunch.  Would you like some?  Yes or No"
+answer = gets.chomp
+
+puts "Would you like to be enrolled in our company's health insurance?  Yes or No"
+response = gets.chomp
+
