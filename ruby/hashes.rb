0fa646e945fc58f69a71a8bde65c9b6e9423ec64
Peter Thompson <peterthompson@roamflyfishing.com>
2017-04-07 12:19:25 -0600

    Delete redundent comments.

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -62,25 +62,4 @@ end
 #Print Keys and Values in user friendly version.
 puts "Your client info: \n Name: #{interior_design[:name]}\n Age: #{interior_design[:age]}\n Children: #{interior_design[:children]}\n Likes moder design: #{interior_design[:design]}"
 
-  
-
-
- 
-  
-#puts "Here is the content that was in the hash: "
-#interior_design do |key, value|
-#puts "#{key}, #{value}"
-#end
- 
-  #Print the hash back out to the screen when the designer has answered all of the questions.
-
-  #Give the user the opportunity to update a key (no need to loop, once is fine). 
-  #After all, sometimes users make mistakes! If the designer says "none", skip it. 
-  #But if the designer enters "decor_theme" (for example), your program should ask 
-  #for a new value and update the :decor_theme key. 
-  #(Hint: Strings have methods that will turn them into symbols, 
-  #which would be quite handy here.) You can assume the user will correctly input 
-  #a key that exists in your hash -- no need to handle user errors.
-
-  #Print the latest version of the hash, and exit the program.
 
