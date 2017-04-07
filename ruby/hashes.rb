8a83de79e61a685bf22edf1e383caa5f897a0347
Peter Thompson <peterthompson@roamflyfishing.com>
2017-04-07 10:49:03 -0600

    Begin creating program that takes user input and places into hash.

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,22 +1,42 @@
-#CREATE PROGRAM THAT TAKES DETAIL INPUT OF CLIENTS: STRINGS, INTEGARS, BOOLEANS.
-#USE HASHES TO MAKE KEYS AND THEIR REPRESENTED VALUE.
+#CREATE PROGRAM THAT TAKES INPUT OF CLIENTS: STRINGS, INTEGARS, BOOLEANS.
+#USE HASH TO MAKE KEYS AND THEIR REPRESENTED VALUE.
 #Program should ...
 
-  #Prompt the designer/user for all of this information.
-  puts "What is the clients full name?"
-  name = gets.chomp
+#Create hash with keys but leave values empty.
+interior_design = {
+  name: "",
+  age: "".to_i,
+  children: "".to_i, 
+  design: ""
+}
 
-  puts "What is the clients age?"
-  age = gets.to_i 
+#Prompt the designer/user for all of this information.
+#And convert any user input to the appropriate data type.
+puts "What is the clients full name?"
+name = gets.chomp
+name << interior_design[:name]
+puts "What is the clients age?"
+age = gets.to_i
+age << interior_design[:age]
+puts "Number of children?"
+children = gets.to_i
+children << interior_design[:children]
+puts "Does client like modern design?"
+design = gets.chomp 
+if design == "yes"
+  design = true
+else 
+  false
+end 
+design << interior_design[:design]
 
-  puts "Number of children?"
-  children = gets.to_i
 
-  puts "Does client like modern design?"
-  design = gets.chomp 
-
+ 
   
-  #Convert any user input to the appropriate data type.
+#puts "Here is the content that was in the hash: "
+#interior_design do |key, value|
+#puts "#{key}, #{value}"
+#end
  
   #Print the hash back out to the screen when the designer has answered all of the questions.
 
@@ -29,3 +49,4 @@
   #a key that exists in your hash -- no need to handle user errors.
 
   #Print the latest version of the hash, and exit the program.
+
