90f65f2c2f97792d983db1b905422c8609e15688
Peter Thompson <peterthompson@roamflyfishing.com>
2017-04-12 15:54:21 -0600

    Fix attribute-changing methods.  Add att_reader and attr_accessors.  Comment out data-structure for Santas.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,10 +1,12 @@
 class Santa
-  def initialize(gender, ethnicity)
+  attr_reader :ethnicity, :age
+  attr_accessor :gender
+  def initialize(genders, ethnicity)
     p "Initializing Santa instance..."
-    @gender = gender
+    @genders = genders
     @ethnicity = ethnicity
     @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", 
-      "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+     "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
     @age = 0
   end 
 
@@ -15,18 +17,43 @@ class Santa
   def eat_milk_and_cookies(type)
     p "That was a good #{type}!"
   end
+
+  def celebrate_birthday
+    @age = @age + rand(141)
+  end 
+
+  def get_mad_at(reindeer)
+    @reindeer_ranking.delete(reindeer)
+    @reindeer_ranking.push(reindeer)
+  end
 end 
-  
-santas = []
-genders = ["female", "male", "female", "bigender", "gender fluid"]
-ethnicities = ["black", "Latino", "white", "Asian", "Mongolian"]
 
-genders.length.times do |i|
-  santas << Santa.new(genders[i], ethnicities[i])
-end
 
-santas.each do |santa|
+genders = ["female", "male", "half-n-half", "bi-gender", "gender fluid"]
+ethnicities = ["Black", "Latino", "White", "Asian", "Mongolian"]
+
+index = 0
+while index < 100
+  #santas = []
+  santa = Santa.new(genders.sample, ethnicities.sample)
+  #santas << santa 
+  #santas.each do 
   santa.eat_milk_and_cookies("chocolate chip cookie")
-  
-  
+  santa.speak
+  santa.get_mad_at("Dasher")
+  santa.celebrate_birthday
+  santa.gender = "So fly"
+  santa.age
+  santa.ethnicity
+  puts "I'm a #{genders.sample} and #{ethnicities.sample}!"
+    #end
+  index += 1
 end
+
+
+
+
+
+
+
+
