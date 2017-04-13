e25847848c7b57bc279acdd2a69d97c07e5079a5
Peter Thompson <peterthompson@roamflyfishing.com>
2017-04-12 20:34:56 -0600

    Change standalone module to mixin module.  Add two classes and add driver code.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,30 @@
 module Shout
-  def self.yell_angrily(words)
+  #def self.yell_angrily(words)
+  #  words + "!!!" + " :("
+  #end
+  #def self.yelling_happily(words)
+  #  words + "!!!!" + " :)"
+  #end 
+  # we'll put some methods here soon, but this code is fine for now!
+  def yell_angrily(words)
     words + "!!!" + " :("
   end
-  def self.yelling_happily(words)
+  def yelling_happily(words)
     words + "!!!!" + " :)"
-  end 
-  # we'll put some methods here soon, but this code is fine for now!
+  end
+end
+#p Shout.yell_angrily("Aaaah Shit")
+#p Shout.yelling_happily("OOOOHHHH SSSHIIIT")
+class DBC_instructor
+  include Shout
 end
 
-p Shout.yell_angrily("Aaaah Shit")
-p Shout.yelling_happily("OOOOHHHH SSSHIIIT")
+class My_wife
+  include Shout
+end
+
+dbc_instructor = DBC_instructor.new
+puts dbc_instructor.yell_angrily("You stupid idiot you forgot to make a feature branch")
+
+my_wife = My_wife.new
+puts my_wife.yelling_happily("I would never yell at you husband")
