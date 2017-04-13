27cb6323b48c94551ee4b2ccecf470b23bc13d5a
Peter Thompson <peterthompson@roamflyfishing.com>
2017-04-12 20:16:46 -0600

    Create two methods inside module.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,12 @@
 module Shout
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+  def self.yelling_happily(words)
+    words + "!!!!" + " :)"
+  end 
   # we'll put some methods here soon, but this code is fine for now!
 end
+
+p Shout.yell_angrily("Aaaah Shit")
+p Shout.yelling_happily("OOOOHHHH SSSHIIIT")
