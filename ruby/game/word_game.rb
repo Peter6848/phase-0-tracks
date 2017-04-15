619ffbeb72b8f784da42ec7501fcee9e68282023
Peter Thompson <peterthompson@roamflyfishing.com>
2017-04-14 21:53:00 -0600

    Begin adding methods to class and get initialize to work.

--- ruby/game/word_game.rb
+++ ruby/game/word_game.rb
@@ -1,6 +1,6 @@
-#WORD GUESSING GAME: 
-#
-#USER ONE INPUTS A WORD
+
+
+
 #
 #USER TWO GUESSES THE WORD ONE CHARACTER AT A TIME
 #
@@ -13,5 +13,53 @@
 #IF USER TWO GUESSES WORD CORRECTLY THEY GET A CONGRATS
 #
 #IF USER TWO GUESSES INCORRECTLY THEY GET A TAUNT
+#WORD GUESSING GAME: 
 
-sdf 
+#GAME CLASS
+class WordGame
+  attr_reader :get_word
+  attr_reader :guess_count 
+  attr_reader :game_over
+
+  def initialize
+    @get_word = "hello"
+    @guess_count = 0
+    @game_over = false
+  end
+
+  def secret_word(word)
+    word.length
+  end
+
+  def hide_word(word)
+
+
+  end 
+
+  def guess_letter
+  end
+
+  def print_letter
+  end 
+
+#USER ONE INPUTS A WORD
+end
+
+#USER INTERFACE
+
+puts "Lets play the guessing game!"
+game = WordGame.new
+
+puts "Player one please give a word!"
+word = gets.chomp 
+
+
+
+
+
+
+
+
+
+
+
--- ruby/game/word_game_spec.rb
+++ ruby/game/word_game_spec.rb
@@ -1,1 +1,12 @@
-s 
+require_relative 'word_game'
+
+describe WordGame do 
+  let(:word) { WordGame.new }
+
+  it "stores the word given on initialization" do
+    expect(word.get_word).to eq ("hello")
+  end 
+
+
+end
+
