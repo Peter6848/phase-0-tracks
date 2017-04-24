baa766ac6a39c3e5baf98825d1c9a1438a7868f6
Peter Thompson <peterthompson@roamflyfishing.com>
2017-04-24 17:29:09 -0600

    Create a reverse function that reverses a given string.  Add a variable to hold given strings and print variable if length of variable is equal to 3.

--- js/explore.js
+++ js/explore.js
@@ -1,2 +1,23 @@
 // CREATE FUNCTION THAT TAKES STRING AS PARAMETER
 // ITERATE THROUGH EACH CHARACTER AND PRINT THE GIVEN STRING IN REVERSE
+
+//function reverse(string) {
+//  for (var i = 0; i < string.length; i++) {
+//    console.log(string[i]);
+//  }
+//}
+
+function reverse(string) {
+  //if s
+  return string.split('').reverse().join('');
+
+}
+var reversedStrings = []
+
+reversedStrings.push(reverse("hello there"));
+reversedStrings.push(reverse("DOGS"));
+reversedStrings.push(reverse("AAAHHHAAA!!!"));
+
+if (reversedStrings.length == 3) {
+console.log(reversedStrings);
+}
