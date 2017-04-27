60538229b2b954676ba7fa99042279b1b9fbb24b
Peter Thompson <peterthompson@roamflyfishing.com>
2017-04-27 17:03:54 -0600

    Create loop for interior design client's and store key/values in hash.

--- ruby/hashes_1.rb
+++ ruby/hashes_1.rb
@@ -10,4 +10,48 @@
 
 #METHOD FOR INTERIOR DESIGNER
 
-  puts ""
+puts "How many client sheets would you like to fill out today?"
+amount = gets.to_i 
+
+for i in 1..amount
+  client_info = {}
+
+  puts "What is clients name:"
+  name = gets.chomp! 
+  client_info[:name] = name 
+
+  puts "What is clients age:"
+  age = gets.to_i
+  client_info[:age] = age
+
+  puts "How many children does client have:"
+  children = gets.to_i
+  client_info[:children] = children 
+
+  puts "Does client like light shades:" 
+  light_shade = gets.chomp.downcase 
+  if light_shade == "yes"
+    client_info[:light_shade] = true
+  else
+    client_info[:light_shade] = false 
+  end 
+
+  client_info.each do |key, value|
+    puts "#{key}: #{value}"  
+    end 
+
+  puts "Update info?  If yes please type which item you would like to change otherwise type 'done'."
+  input = gets.chomp
+  if input == 'done' 
+    break 
+  elsif client_info.include? input.to_sym 
+    puts "What is the new value?"
+    value = gets.chomp 
+    client_info[input.to_sym] = value 
+  end 
+
+   client_info.each do |key, value|
+    puts "#{key}: #{value}"  
+    end 
+
+end  
