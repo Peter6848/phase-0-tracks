4e9cc7cf56080ee9c11cd841a0a3c747665328b1
Peter Thompson <peterthompson@roamflyfishing.com>
2017-04-27 16:02:24 -0600

    Write pseudocode for interior designer method.

---
+++ ruby/hashes_1.rb
@@ -0,0 +1,13 @@
+#PSEUDOCODE FOR INTERIOR DESIGNER METHOD
+ # - For each client of designer
+  # - Take all info of client
+  # - Convert info to keys and values within hash  
+  # - Keys should be symbols unless string is needed
+  # - Print the hash when all answers of questions are filled
+  # - Give option to update any key/value once
+   # - If no update needed allow user to type 'done' to skip 
+  # - Print updated hash and exit program
+
+#METHOD FOR INTERIOR DESIGNER
+
+  puts ""
