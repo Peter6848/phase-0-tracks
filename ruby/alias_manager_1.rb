a21897b89fd2f632df6f162b12d558697eea8151
Peter Thompson <peterthompson@roamflyfishing.com>
2017-04-27 19:31:05 -0600

    Create pseudocode for spy name.

---
+++ ruby/alias_manager_1.rb
@@ -0,0 +1,11 @@
+#PSEUDOCODE FOR SPY NAME
+
+# - For each spy 
+ # - Ask for the spy's real name
+ # - Take spy's real name
+ # - Swap the first and last name
+ # - Change all of the vowels to the next vowel
+ # - Change all consonants to the next consonant
+ # - Leave any spaces as is
+
+ 
