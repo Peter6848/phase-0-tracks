3d845128e3db3b9e8d9dc828a7c31b1635f1e28b
Peter Thompson <peterthompson@roamflyfishing.com>
2017-04-29 20:33:56 -0600

    Use built in methods to iterate through array and hahsh.

--- ruby/iteration_1.rb
+++ ruby/iteration_1.rb
@@ -10,12 +10,62 @@ animals = ["cat", "dog", "cow", "chicken"]
 
 my_family = {brother_1: "steven", brother_2: "Eric", sister: "Justine", father: "Steve", mother: "Helen"}
 
+p animals
 animals.each {|animal| puts "#{animal}"}
+p animals
 
+p my_family
 my_family.each do |sibling, name|
   puts "#{sibling}: #{name}"
 end
+p my_family
 
+p animals
 animals.map! do |animal|
   puts "I'm a #{animal}!!!"
 end
+p animals
+
+DTT = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+DTT.drop_while do |i| 
+    i < 5
+end 
+
+DTT.keep_if do |i|
+    i < 5
+end
+
+DTT.select { |i|
+    i.even?
+}
+
+DTT.delete_if do |i|
+    i >= 8
+end
+
+#HASH USING NEW FOUND METHODS
+DTT = {
+    "Cat" => "Kitten",
+    "Dog" => "Puppy",
+    "Goat" => "Kid",
+    "Kangaroo" => "Joey"
+}
+
+DTT.delete_if { |animal, baby| 
+    animal >= "Goat"
+}
+
+p DTT
+
+DTT.keep_if { |animal, baby|
+    baby == "Kid"
+}
+
+p DTT
+
+DTT.reject { |animal, baby|
+    animal < "Dog"
+}
+
+p DTT
