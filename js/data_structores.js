c233aca2ab7fe063ce2dd95d190cf2d531b7c13b
Peter Thompson <peterthompson@roamflyfishing.com>
2017-05-12 14:57:24 -0700

    Create file with two arrays.  Use .push to add additional elements into arrays.

---
+++ js/data_structores.js
@@ -0,0 +1,9 @@
+var colors = ["Purple", "Red", "Green", "Yellow"];
+
+var horsies = ["Carlton", "Pedro", "Pickles", "Bob"];
+
+colors.push("Blue");
+horsies.push("Shit Face");
+
+console.log(colors);
+console.log(horsies);
