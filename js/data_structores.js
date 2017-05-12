6deb064a0cb060533afa87f0fb837b8a2899d135
Peter Thompson <peterthompson@roamflyfishing.com>
2017-05-12 15:49:18 -0700

    Create constructor function with one function.

--- js/data_structores.js
+++ js/data_structores.js
@@ -7,3 +7,25 @@ horsies.push("Shit Face");
 
 console.log(colors);
 console.log(horsies);
+
+var key = horsies[0];
+var obj = {};
+obj[key] = colors[0];
+
+console.log(obj);
+
+function Car(year, make, isPieceOfCrap) {
+
+  console.log("Your new car:", this);
+
+  this.year = year;
+  this.make = make;
+  this.isPieceOfCrap = isPieceOfCrap;
+
+  this.breakDownAgain = function() { console.log("Stupid piece of shit car!"); };
+
+}
+
+var myCar = new Car(2003, "Subaru", true);
+console.log(myCar);
+myCar.breakDownAgain();
