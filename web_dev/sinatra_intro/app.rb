18e5bd6bed1667259c1131ab6d075fbbd4ba4e13
Peter Thompson <peterthompson@roamflyfishing.com>
2017-05-26 22:37:39 -0700

    Begin creating GET routes for each question.

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -44,3 +44,46 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+# Write a GET /contact route that displays an address (you can make up the address).
+
+get '/address/:contact' do
+  contact = params[:contact]
+  "#{params[:contact]}"
+end
+
+# A /great_job route that can take a person's name as a query parameter 
+# (not a route parameter) and say "Good job, [person's name]!". 
+# If the query parameter is not present, the route simply says "Good job!"
+
+get '/great_job/' do
+  name = params[:name]
+  if name 
+    "Good job #{params[:name]}!"
+  else
+    "Good job!"
+  end
+end
+
+#A route that uses route parameters to add two numbers and respond with the result. 
+# The data types are tricky here -- when will the data need to be (or arrive as) a string?
+
+# Optional bonus: Make a route that allows the user to search the database in some way 
+# -- maybe for students who have a certain first name, or some other attribute. 
+# If you like, you can simply modify the home page to take a query parameter, 
+# and filter the students displayed if a query parameter is present.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
